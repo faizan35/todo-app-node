b529abcac6a80d58c273ac8bae00dffe3f9dfed3
Faizan <faizanansari4862@gmail.com>
2023-11-18 19:54:29 +0530

    app.js done

---
+++ app.js
@@ -0,0 +1,22 @@
+// app.js
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+// Serve static files from the 'public' directory
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+routes(app);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
